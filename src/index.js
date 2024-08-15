23b4ed9257e1bf954d98053e2be13cd7ef9b821d
Aiden Cullo <aidencullo@gmail.com>
2024-08-15 16:41:13 -0400

    The username cannot contain any special characters or whitespace.
    The email must be a valid email address.
    The email must not be from the domain "example.com."

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 const registration = document.getElementById("registration");
 const username = registration.elements["username"];
+const email = registration.elements["email"];
 
 function validateUsername(evt) {
-  let usernameVal = username.value;
+  const usernameVal = username.value;
   const uniqueChars = new Set(usernameVal);
-
   if (uniqueChars.size < 2) {
     alert(
       "Your username must contain at least two unique characters."
     );
     username.focus();
     evt.returnValue = false;
-    return false; 
+    return false;
  }
+  evt.returnValue = true;
+  return usernameVal;
+}
+
+function validateEmail(evt) {
+  const emailVal = email.value;
+  if (emailVal.includes("example.com")) {
+    alert(
+      "Your email address must not contain 'example.com'."
+    );
+    email.focus();
+    evt.returnValue = false;
+    return false;
+  }
+  evt.returnValue = true;
+  return emailVal;
 }
 
 registration.addEventListener("submit", validateUsername);
+registration.addEventListener("submit", validateEmail);
